0454f77cee3b8a574443be694dab9d11b78356a5
Patricknshimiyimana <lennoxmupi@gmail.com>
2020-11-11 20:20:43 +0200

    changed things about styling and responsiveness

---
+++ js/validation.js
@@ -0,0 +1,18 @@
+
+
+// login form validation
+
+function validateLogin() {
+    let email = document.getElementById('email-input').value,
+        password = document.getElementById('password-input').value
+
+if (email === '' || password === '') {
+    alert('please enter your email and password');
+} else {
+    // submit form and login
+    // alert('thank you, your message received');
+}
+}
+
+
+
