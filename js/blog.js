1ef753febe26168a7af0b26b62c6f725162628f2
Patricknshimiyimana <lennoxmupi@gmail.com>
2020-11-16 18:59:09 +0200

    create the post with image and store it to firestore and display to the browser

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,6 @@
 
 const postWrap = document.querySelector(".wrap-posts");
+const userComment = document.querySelector(".form");
 
 // create element and render posts to the ui
 
@@ -9,13 +10,18 @@ function renderPost(doc) {
     // let postImage = document.createElement('img');
     let postTitle = document.createElement('h2');
     let postBody = document.createElement('p');
+    let postImage = document.createElement('img');
 
     postDiv.setAttribute('data-id', doc.id);
+    postDiv.setAttribute('class', 'post');
     postTitle.textContent = doc.data().title;
     postBody.textContent = doc.data().body;
+    postImage.src = doc.data().image;
 
+    postDiv.appendChild(postImage);
     postDiv.appendChild(postTitle);
     postDiv.appendChild(postBody);
+
 
     postWrap.appendChild(postDiv);
 
@@ -28,3 +34,34 @@ db.collection('posts').get().then((snapshot) => {
         renderPost(doc);
     })
 })
+
+// saving the data (comments) to the firestore
+
+// userComment.addEventListener('click', (e) => {
+//     // e.preventDefault();
+//     console.log('helooo')
+// })
+
+// let commentContent = document.getElementById('comment-content');
+//     commentButton = document.getElementById('comment-btn')
+
+// //handle successful upload
+// let today = new Date();
+// let dateOfToday = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+// uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL){
+//     db.collection('user-comments').add({
+//         // image: downloadURL,
+//         // title: postTitle.value,
+//         content: commentContent.value,
+//         date: dateOfToday
+//     }).then(function(){
+//         alert('Successfuly uploaded!');
+        
+//         window.location.href = "dashboard.html";
+//     })
+//     .catch(function(error) {
+//         alert('Error uploading post, Try again!');
+//     });
+    
+    
+// });
